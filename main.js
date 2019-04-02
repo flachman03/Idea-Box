69bedca1dfd1d6cb83cf804b573c5e77bbe9c5f9
RyanFlachman <ryan@flachman.net>
2019-04-02 12:30:28 -0600

    Add query selectors

---
+++ main.js
@@ -0,0 +1,21 @@
+/*----------------Query Selectors---------------*/
+
+var starredIdeas = document.querySelector('#sidebar-starred-ideas-btn');
+var newQualityInput = document.querySelector('#new-quality');
+var newQualityBtn = document.querySelector('#new-quality-btn');
+var titleInput = document.querySelector('#title-input');
+var bodyInput = document.querySelector('#bodyInput');
+var saveBtn = document.querySelector('#save-btn');
+
+/*----------------Event Listeners---------------*/
+
+newQualityBtn.addEventListener('click', function(){
+
+})
+
+saveBtn.addEventListener('click', function() {
+
+})
+
+
+/*-----------------functions--------------------*/
