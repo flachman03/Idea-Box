947c8c909c00f2df660df8801b1d1df53aef9044
RyanFlachman <ryan@flachman.net>
2019-04-02 16:51:29 -0600

    Add idea array

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var newQualityBtn = document.querySelector('#new-quality-btn');
 var titleInput = document.querySelector('#title-input');
 var bodyInput = document.querySelector('#body-input');
 var saveBtn = document.querySelector('#save-btn');
+var ideaArray = [];
 
 /*----------------Event Listeners---------------*/
 
@@ -22,18 +23,24 @@ saveBtn.addEventListener('click', function() {
 
 function saveNewObject() {
 	var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
+	ideaArray.push(newIdea);
+	console.log(ideaArray);
 	var stringified = JSON.stringify(newIdea);
 	localStorage.setItem(newIdea.id, stringified);
 	console.log(localStorage);
 }
 
-function getObject(idea) {
-	var storedIdea = localStorage.getItem(idea.id);
+function getObject(ideaArray) {
+	ideaArray.forEach(function(item) {
+	var storedIdea = localStorage.getItem(item.id);
 	var parsed = JSON.parse(storedIdea);
 	return parsed = new Idea(parsed.id, parsed.title, parsed.body);
+	});
 }
 
 function clearInputs() {
 	titleInput.value = '';
 	bodyInput.value = '';
 }
+
+// getObject(localStorage);
